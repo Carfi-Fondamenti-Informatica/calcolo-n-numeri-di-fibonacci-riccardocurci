28f42cca746e092b22b7b9fe7cb4c3cae80f0224
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-13 10:39:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int n=0;
+   cin>>n;
+   
+   if(n<2){
+      cout<<"errore"<< endl;
+      }else{
+      cout<<"1"<<endl;
+      int a=0,b=1,c=0;
+      for(int i=0;i<=(n-2);i++){
+       c=a+b;
+       a=b;
+       b=c;
+       cout<<"c"<<endl;
+       }
+}       
    return 0;
 }
