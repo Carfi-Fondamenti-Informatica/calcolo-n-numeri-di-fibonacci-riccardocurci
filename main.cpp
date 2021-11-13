462f667b4105ffbe28cf1ed3da03c454111ad125
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-13 10:48:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@ int main() {
       cout<<"errore"<< endl;
       }else{
       cout<<"1"<<endl;
-      int n=0,x1=0,x2=1,x3=0;
-      for(int i=0;i<=(n-2);i++){
-       cout<<"c"<<endl;
+      int x1=0,x2=1,x3=0;
+      
+      for(int i=0;i>=(n-2);i++){
        x3=x1+x2;
        x1=x2;
        x2=x3;
+       cout<<"c"<<endl;
        }
 }       
    return 0;
