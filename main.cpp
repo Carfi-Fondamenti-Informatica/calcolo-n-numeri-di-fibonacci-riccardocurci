b9681f0573f6eb10d4fe83a1ad707d0c1fec8f43
riccardocurci <91483368+riccardocurci@users.noreply.github.com>
2021-11-14 11:31:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int n=0 a=1,b=1;
-   cin>>n;
-   cout<<a<<endl;
-   cout<<b<<endl;
-  
-   for(int i=1;i<=n;i++){
-   somma=a+b;
-   cout<<"somma"<<endl;
-   a=b;
-   b=somma;
-}       
-   return 0;
-}
+int main(){
+    int n=0;
+    cin>>n;
+    if(n<2){
+        cout<<"errore"<< endl;
+    }else{
+            cout<<"1"<<endl;
+    }for(int i=0;i<=n-2;i++){
+            int a=0,b=1,c=0;
+            cout<<"c"<<endl;
+            a=b;
+            b=c;
+            c=a+b;
+        }
+    return 0;
